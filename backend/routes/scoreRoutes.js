353f29f53697bed2e626e8f684f88b7825ce84ed
Elina <elinasomnath@gmail.com>
2025-11-27 01:06:03 +0530

    backend compatible with frontend

--- backend/routes/scoreRoutes.js
+++ backend/routes/scoreRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { simulateCarbon } = require("../controllers/scoreControllers");
 
 router.post("/calculate", (req, res) => {
-    const { runtime, memory, loc, cloud } = req.body; // include cloud
-    const result = simulateCarbon(runtime, memory, loc, cloud); // pass cloud
+    const { runtime, memory, loc, cloud } = req.body; 
+    const result = simulateCarbon(runtime, memory, loc, cloud); 
     res.json(result);
 });
 
